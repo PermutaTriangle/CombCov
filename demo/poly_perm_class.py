e627261aa2bcc65a4fa79ae28e64006c71a9f002
Christian Bean <christianbean@ru.is>
2019-06-06 13:22:54 +0000

    pepping!

--- demo/poly_perm_class.py
+++ demo/poly_perm_class.py
@@ -5,7 +5,6 @@ from permuta import Av, Perm
 from tilings import Obstruction, Requirement, Tiling
 
 
-
 class CombCovTiling(Rule):
     def __init__(self, tiling):
         self.tiling = tiling
@@ -18,26 +17,30 @@ class CombCovTiling(Rule):
     def get_subrules(self):
         maxlen = 4
         if self.tiling.dimensions != (1, 1) or self.tiling.requirements:
-            raise NotImplementedError("Only handle 1x1 tilings without requirements :(")
+            raise NotImplementedError(
+                            "Only handle 1x1 tilings without requirements :(")
 
         def blocks(cell):
             incr = Obstruction.single_cell(Perm((1, 0)), cell)
             decr = Obstruction.single_cell(Perm((0, 1)), cell)
             point = [Requirement.single_cell(Perm((0,)), cell)]
-            return [([incr], [point]), ([decr], [point]), ([incr, decr], [point])]
+            return [([incr], [point]), ([decr], [point]),
+                    ([incr, decr], [point])]
 
         basis = [ob.patt for ob in self.tiling.obstructions]
         res = []
         for i in range(1, maxlen + 1):
             for perm in Av(basis).of_length(i):
                 cells = [(i, v) for i, v in enumerate(perm)]
-                for assignment in itertools.product(*[blocks(cell) for cell in cells]):
+                for assignment in itertools.product(*[blocks(cell)
+                                                    for cell in cells]):
                     obstructions = []
                     requirements = []
                     for obs, reqs in assignment:
                         obstructions.extend(obs)
                         requirements.extend(reqs)
-                    res.append(CombCovTiling(Tiling(obstructions, requirements)))
+                    res.append(CombCovTiling(
+                                        Tiling(obstructions, requirements)))
         print("Number of rules is {}.".format(len(res)))
         return res
 
@@ -54,8 +57,8 @@ def main():
 
     max_elmnt_size = 7
 
-    print("Trying to find a cover for \n{}\n using elements up to size {}.".format(
-        cctiling, max_elmnt_size))
+    print(("Trying to find a cover for \n{}\n using elements up to size {}."
+           "".format(cctiling, max_elmnt_size)))
     comb_cov = CombCov(cctiling, max_elmnt_size)
     comb_cov.solve()
 
