e8b18c5048adbbaf9af2d6e376f2ee0a1090e4c1
Bjarni Jens Kristinsson <bjarni.jens@gmail.com>
2019-09-03 18:16:30 -0400

    Patch version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="CombCov",
-    version="0.4.0",
+    version="0.4.1",
     author="Permuta Triangle",
     author_email="permutatriangle@gmail.com",
     description="Searching for combinatorial covers.",
