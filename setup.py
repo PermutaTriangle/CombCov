3a17cf004632d6992dfa7c672f2f931e1efb1fad
requires.io <support@requires.io>
2020-04-05 18:36:16 -0400

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=[
         "permuta==1.3.0",
-        "PuLP==2.0",
+        "PuLP==2.1",
     ],
     setup_requires=["pytest-runner==5.2"],
     tests_require=[
