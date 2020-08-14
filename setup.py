6723102e4c9733b8f2600597d0d68bef1a59fbab
requires.io <support@requires.io>
2020-08-14 17:54:33 +0000

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     setup_requires=["pytest-runner==5.2"],
     tests_require=[
         "pytest==6.0.1",
-        "pytest-cov==2.10.0",
+        "pytest-cov==2.10.1",
         "pytest-pep8==1.0.6",
         "pytest-isort==1.1.0",
     ],
